1b1f61be17e7bc3d3a0e68b205ae947bc38741ba
antonshishkin92 <antonshishkin92@gmail.com>
2021-11-15 12:49:14 +0400

    lesson 4: домашнее задание. Вычисление квадратного корня без применения метода Math.sqrt

---
+++ src/main/java/lesson4/SquareRoot.java
@@ -0,0 +1,22 @@
+package lesson4;
+
+public class SquareRoot {
+    public static void main(String[] args) {
+        calcroot();
+    }
+
+    public static void calcroot() {
+        double number = 25, guess = 10;
+        double result, resultnew, endresult;
+        result = (guess + (number / guess)) / 2; /** Эта формула называется метод Ньютона, для вычисления
+         квадратного корня числа number, result - первое предположение корня числа number, guess - любое число наугад */
+        resultnew = (result + (number / guess)) / 2;
+        /** resultnew - новое предположение корня числа number */
+        endresult = (resultnew + (number / resultnew)) / 2;
+        System.out.println("Результат вычисления: " + result);
+        System.out.println("Новый результат: " + resultnew);
+        System.out.println("Квадратный корень: " + endresult);
+        /** endresult - окончательный квадратный корень числа number */
+    }
+}
+
