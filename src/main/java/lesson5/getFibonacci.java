e1431fad8270a6e0aa13764bd772efeaa6281613
antonshishkin92 <antonshishkin92@gmail.com>
2021-11-18 18:18:49 +0400

    lesson 5: Получение n-го по счёту числа Фибоначи

--- src/main/java/lesson5/getFibonacci.java
+++ src/main/java/lesson5/getFibonacci.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class getFibonacci {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите n-ую последовательность: ");
+        System.out.println("Введите n-ую последовательность числа Фибоначчи: ");
         int n = scanner.nextInt();
         int a = 0;
         int b = 1;
