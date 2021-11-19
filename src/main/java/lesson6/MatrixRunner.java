d4feee0c382c160cd25b0249353616a914c736fb
antonshishkin92 <antonshishkin92@gmail.com>
2021-11-20 00:07:33 +0400

    lesson 6: Массивы

---
+++ src/main/java/lesson6/MatrixRunner.java
@@ -0,0 +1,29 @@
+package lesson6;
+
+import java.util.Arrays;
+
+public class MatrixRunner {
+    public static void main(String[] args) {
+        int[][] matrix = new int[3][3];
+        fillFigure(matrix);
+        // System.out.println(Arrays.deepToString(matrix));
+        printFigure(matrix);
+    }
+
+    public static void fillFigure(int[][] array) {
+        for (int i = 0; i < array.length; i++) { // перебор строк таблицы
+            for (int j = 0; j < array[i].length; j++) {// перебор столбцов таблицы
+                if (i == j || j== array[i].length-1){
+                    array[i][j] = 1;
+                }
+
+            }
+        }
+    }
+
+    public static void printFigure(int[][] matrix) {
+        for (int i = 0; i <array.length; i++) {
+            System.out.println(Arrays.toString(array[i]));
+        }
+    }
+}
---
+++ src/main/java/lesson6/WordReducerRunner.java
@@ -0,0 +1,40 @@
+package lesson6;
+
+import java.util.Arrays;
+
+public class WordReducerRunner {
+    public static void main(String[] args) {
+        String[] words = {"Большой", "привет", "от", "слушателей", "ИТ парка", "от" };
+        System.out.println(Arrays.toString(Intersect(words, "от")));
+        System.out.println(Arrays.toString(Intersect(words, "Привет")));
+    }
+
+    public static String[] Intersect(String[] initialArray, String word) {
+        String[] result = new String[initialArray.length];
+        System.arraycopy(initialArray, 0, result, 0, initialArray.length);
+        for (int i = 0; i < result.length; i++) {
+            if (result[i].equals(word)) {
+                result[i] = null;
+            }
+        }
+
+        int maxNotNullIndexes = 0;
+        for (String str : result) {
+            if (str != null) {
+                maxNotNullIndexes++;
+            }
+        }
+
+        String[] newResult = new String[maxNotNullIndexes];
+        int counter = 0;
+        for (String str : result) {
+            if (str != null) {
+                newResult[counter++] = str;
+            }
+
+
+        }
+
+        return newResult;
+    }
+}
