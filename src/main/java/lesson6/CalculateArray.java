9067f7b1dc545581c3586ac832a371c7ffe27e93
antonshishkin92 <antonshishkin92@gmail.com>
2021-11-22 16:13:34 +0400

    lesson 6: Массивы

---
+++ src/main/java/lesson6/CalculateArray.java
@@ -0,0 +1,33 @@
+package lesson6;
+
+import java.util.Scanner;
+
+public class CalculateArray {
+    public static void main(String[] args) {
+        int n = 100;
+        double[] array = new double[n];
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Укажите длину массива: ");
+        int a = scanner.nextInt();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Math.random();
+        }
+
+
+        double max = array[0];
+        double min = array[0];
+        double avg = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (max < array[i])
+                max = array[i];
+            if (max > array[i])
+                min = array[i];
+            avg += array[i] / array.length;
+        }
+
+        System.out.println("Максимальное значение " + max);
+        System.out.println("Минимальное значение " + min);
+        System.out.println("Среднее значение " + avg);
+    }
+}
+
