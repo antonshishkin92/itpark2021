0d5a8ba399f0ecb66a19b1bd573a6b842104f77c
antonshishkin92 <antonshishkin92@gmail.com>
2021-11-24 18:10:52 +0400

    lesson 7: Домашнее задание

---
+++ src/main/java/lesson7/Animal.java
@@ -0,0 +1,21 @@
+package lesson7;
+
+public class Animal {
+    String name;
+    int age;
+    double weight;
+    int swimming;
+    int running;
+
+    public Animal() {
+
+    }
+
+    public Animal(String name, int age, double weight, int swimming, int running) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.swimming = swimming;
+        this.running = running;
+    }
+}
---
+++ src/main/java/lesson7/AnimalRunner.java
@@ -0,0 +1,21 @@
+package lesson7;
+
+public class AnimalRunner {
+
+    public static void main(String[] args) {
+        Animal cat = new Animal();
+        cat.name = "Tosha";
+        cat.age = 8;
+        cat.weight = 4.5;
+        cat.running = 130;
+        cat.swimming = 0;
+
+        Animal dog = new Animal();
+        dog.name = "Tosha";
+        dog.age = 3;
+        dog.weight = 6.2;
+        dog.running = 480;
+        dog.swimming = 0;
+
+    }
+}
