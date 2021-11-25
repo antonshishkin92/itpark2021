7833fe0b423b15e0aebdef088139ef24e0eb8864
antonshishkin92 <antonshishkin92@gmail.com>
2021-11-25 16:44:48 +0400

    lesson 7: Домашнее задание

--- src/main/java/lesson7/AnimalRunner.java
+++ src/main/java/lesson7/AnimalRunner.java
@@ -4,18 +4,28 @@ public class AnimalRunner {
 
     public static void main(String[] args) {
         Animal cat = new Animal();
-        cat.name = "Tosha";
+        cat.name = "Тоша";
         cat.age = 8;
         cat.weight = 4.5;
         cat.running = 130;
         cat.swimming = 0;
 
         Animal dog = new Animal();
-        dog.name = "Tosha";
+        dog.name = "Рекс";
         dog.age = 3;
         dog.weight = 6.2;
         dog.running = 480;
-        dog.swimming = 0;
+        dog.swimming = 9;
+
+
+        Animal[] animals = {cat, dog};
+        for (Animal animal : animals) {
+            if (Integer.valueOf(480).equals(animal.running))
+                System.out.println("Собака по имени " + animal.name + " пробежала " + animal.running + " метров");
+            else if (Integer.valueOf(0).equals(animal.swimming)) {
+                System.out.println(animal.name + " не умеет плавать");
+            }
+        }
 
     }
 }
