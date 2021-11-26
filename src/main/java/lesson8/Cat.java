f57fafe6fd505bde2eaf8deea8d73699f25b7e94
antonshishkin92 <antonshishkin92@gmail.com>
2021-11-26 21:43:47 +0400

    lesson 8:

--- src/main/java/lesson8/Cat.java
+++ src/main/java/lesson8/Cat.java
@@ -8,8 +8,16 @@ public class Cat {
     String poroda;
     String bossName;
 
-    public boolean equals(Cat cat) {
-        return Objects.equals(name, cat.name) && age == cat.age
-                && Objects.equals(poroda, cat.poroda);
+    public boolean equals(Object cat) {
+        if (cat instanceof Cat c) {
+            return Objects.equals(name, c.name) && age == c.age
+                    && Objects.equals(poroda, c.poroda);
+
+        }
+        return false;
     }
+
+      public boolean equals(Cat cat) {
+         return super.equals(cat);
+     }
 }
--- src/main/java/lesson8/CatRunner.java
+++ src/main/java/lesson8/CatRunner.java
@@ -3,17 +3,21 @@ package lesson8;
 public class CatRunner {
     public static void main(String[] args) {
         Cat barsik = new Cat();
-        barsik.name="барсик";
+        barsik.name="Барсик";
         barsik.age=1;
         barsik.poroda="Сибирский";
         barsik.bossName="Антон";
         Cat persik = new Cat();
+        persik.name="Персик";
+        persik.age=4;
+        persik.poroda="Персидский";
+        persik.bossName="Сергей";
         Cat bobik = new Cat();
         Cat malysh = new Cat();
         System.out.println(barsik == persik);
         System.out.println(barsik == bobik);
         System.out.println(barsik == malysh);
-
+        System.out.println("--------------------");
         System.out.println(barsik.equals(persik));
     }
 }
