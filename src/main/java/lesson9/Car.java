f7b75b764134ed377d79166fb51a268bffccf9be
antonshishkin92 <antonshishkin92@gmail.com>
2021-12-22 16:46:32 +0400

    lesson 9:

--- src/main/java/lesson9/Car.java
+++
@@ -1,9 +0,0 @@
-package lesson9;
-
-public class Car extends Automobile{
-
-    public Car(String brand, String model, int distance) {
-        super(brand, model, distance);
-
-    }
-}
--- src/main/java/lesson9/Truck.java
+++
@@ -1,8 +0,0 @@
-package lesson9;
-
-public class Truck extends Automobile{
-
-    public Truck(String brand, String model, int distance) {
-        super(brand, model, distance);
-    }
-}
