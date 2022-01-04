95a5ad570d28d55e77822b09675c2c3b6f095fe3
antonshishkin92 <antonshishkin92@gmail.com>
2022-01-04 22:58:15 +0400

    lesson 9: homework

---
+++ src/main/java/hw9/Automobile.java
@@ -0,0 +1,12 @@
+package hw9;
+
+public class Automobile {
+
+    public String brand;
+    public String model;
+   public double tankCapacity;
+    public double fuelFlow;
+    public double distance;
+
+
+}
