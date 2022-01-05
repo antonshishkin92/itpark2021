bd39e208deaae0a3b1656aa978c5147b02429767
antonshishkin92 <antonshishkin92@gmail.com>
2022-01-05 21:48:57 +0400

    lesson 9: homework

--- src/main/java/hw9/Automobile.java
+++ src/main/java/hw9/Automobile.java
@@ -4,9 +4,32 @@ public class Automobile {
 
     public String brand;
     public String model;
-   public double tankCapacity;
-    public double fuelFlow;
-    public double distance;
 
+
+    public Automobile(String brand, String model) {
+        this.brand = brand;
+        this.model = model;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double distance(double fuelConsumption, double tankCapacity){
+        double distanceWithoutRefueling = (100/fuelConsumption) * tankCapacity;
+return distanceWithoutRefueling;
+    }
 
 }
