b32ca4b8bacaca94c7686d4d87c7927d059de37f
antonshishkin92 <antonshishkin92@gmail.com>
2022-01-06 22:14:55 +0400

    lesson 9: homework

--- src/main/java/hw9/Truck.java
+++ src/main/java/hw9/Truck.java
@@ -1,9 +1,16 @@
 package hw9;
 
-public class Truck extends Automobile{
+public class Truck extends Automobile {
+
+    private double distanceWithoutRefuelingTruck;
 
     public Truck(String brand, String model, double fuelConsumption, double tankCapacity) {
-        super(brand, model, fuelConsumption, tankCapacity,0.0);
+        super(brand, model, fuelConsumption, tankCapacity, 0.0);
+    }
+
+    public void distanceWithoutRefuelingTruck() {
+        distanceWithoutRefuelingTruck = (100 / fuelConsumption) * tankCapacity;
+
     }
 
 
