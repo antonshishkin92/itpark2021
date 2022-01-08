d01cf750a0727b698ae2a0fe248e0d44b6514de0
antonshishkin92 <antonshishkin92@gmail.com>
2022-01-08 23:19:04 +0400

    lesson 9: homework

--- src/main/java/hw9/Car.java
+++ src/main/java/hw9/Car.java
@@ -1,15 +1,10 @@
 package hw9;
 
-public class Car extends Automobile{
-
+public class Car extends Automobile {
 
 
     public Car(String brand, String model, double fuelConsumption, double tankCapacity) {
-        super(brand, model, fuelConsumption, tankCapacity,0.0);
+        super(brand, model, fuelConsumption, tankCapacity, 0.0);
     }
-
-
-
-
 
 }
--- src/main/java/hw9/vehicleRunner.java
+++ src/main/java/hw9/vehicleRunner.java
@@ -2,7 +2,34 @@ package hw9;
 
 public class vehicleRunner {
     public static void main(String[] args) {
-Car hyundaiAccent=new Car("Hyundai","Accent",9.9,45);
+        Car hyundaiAccent = new Car("Hyundai", "Accent", 9.9, 45);
+        Car daewooMatiz = new Car("Daewoo", "Matiz", 13.1, 35);
+        Car chevroletAveo = new Car("Chevrolet", "Aveo", 14.5, 46);
+        Car opelCorsa = new Car("Opel", "Corsa", 7.7, 45);
+        Car kiaRio = new Car("Kia", "Rio", 8.6, 43);
+        Car nissanJuke = new Car("Nissan", "Juke", 7.3, 45);
+        Car skodaFabia = new Car("Skoda", "Fabia", 12.4, 50);
+        Car fordKuga = new Car("Ford", "Kuga", 11.4, 60);
+        Car fiatDucato = new Car("Fiat", "Ducato", 12.6, 80);
+        Car chevroletNiva = new Car("Chevrolet", "Niva", 17.7, 60);
+        Car kiaCeed = new Car("Kia", "Ceed", 9.8, 55);
+        Car toyotaCorolla = new Car("Toyota", "Corolla", 8.2, 50);
+        Car toyotaCamry = new Car("Toyota", "Camry", 10.1, 70);
         System.out.println(hyundaiAccent);
+        System.out.println(daewooMatiz);
+        System.out.println(chevroletAveo);
+        System.out.println(opelCorsa);
+        System.out.println(kiaRio);
+        System.out.println(nissanJuke);
+        System.out.println(nissanJuke);
+        System.out.println(skodaFabia);
+        System.out.println(fordKuga);
+        System.out.println(fiatDucato);
+        System.out.println(chevroletNiva);
+        System.out.println(kiaCeed);
+        System.out.println(toyotaCorolla);
+        System.out.println(toyotaCamry);
+        System.out.println("-------------------------");
+
     }
 }
