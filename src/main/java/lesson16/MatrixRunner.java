f4b753b0c485ebd0361e5d0fe89045b937108809
antonshishkin92 <antonshishkin92@gmail.com>
2022-01-17 17:04:11 +0400

    lesson 16: java.util.consurrent

---
+++ src/main/java/lesson16/MatrixRunner.java
@@ -0,0 +1,12 @@
+package lesson16;
+
+public class MatrixRunner {
+
+    public static void main(String[] args) {
+        Matrix matrixA = new Matrix(2);
+        Matrix matrixB = new Matrix(2);
+        System.out.println(matrixA.sum(matrixB));
+        System.out.println(matrixA.subtract(matrixB));
+    }
+}
+
