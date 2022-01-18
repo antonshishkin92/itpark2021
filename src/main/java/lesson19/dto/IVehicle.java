29484e9a5784caa9d01fb5fb1699fe3f09fe488a
antonshishkin92 <antonshishkin92@gmail.com>
2022-01-18 22:11:54 +0400

    lesson 19: Dynamic proxy + форматы данных

---
+++ src/main/java/lesson19/dto/IVehicle.java
@@ -0,0 +1,9 @@
+package lesson19.dto;
+
+
+public interface IVehicle {
+    double getWidth();
+    Double getHeigth();
+    String getMarkAndModel(Integer digit);
+
+}
--- src/main/java/lesson19/fileRunner.java
+++ src/main/java/lesson19/fileRunner.java
@@ -1,5 +1,8 @@
 package lesson19;
 
+import lesson19.util.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+
 import java.io.File;
 
 public class fileRunner {
@@ -7,9 +10,13 @@ public class fileRunner {
     public static void main(String[] args) {
         String filelocation = "D:\\test\\test.txt";
         File file=new File(filelocation);
+        String fileName=file.getName();
         System.out.println("имя файла " + file.getName());
+        System.out.println("Расширение файла: " + FileUtils.getFileExtension(fileName));
+        System.out.println("Расширение файла: " + FilenameUtils.getExtension(fileName));
         System.out.println( "Родительская папка файла " + file.getParent());
         System.out.println( "Существует файл? " + (file.exists() ? "Да" : "нет"));
+        System.out.println(file.isFile() ? "Да" : "нет");
         System.out.println( "Создались промежуточные папки? " + (file.mkdirs() ? "Да" : "нет"));
 
 
