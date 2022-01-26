7e5ceaba682691bfa9410905664085b2155c729f
antonshishkin92 <antonshishkin92@gmail.com>
2022-01-26 16:41:54 +0400

    lesson 22: java.net

--- src/main/java/lesson22/URIRunner.java
+++ src/main/java/lesson22/URIRunner.java
@@ -1,7 +1,35 @@
 package lesson22;
 
-public class URIRunner {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
 
+public class URIRunner {
+    public static void main(String[] args) throws URISyntaxException, IOException {
+        URI uri = new URI();
+        System.out.println("Протокол:" + uri.getScheme());
+        System.out.println("Хост " + uri.getHost());
+        System.out.println("Порт " + uri.getPort());
+        System.out.println("Путь " + uri.getPath());
+        System.out.println("Параметры " + uri.getRawQuery());
+        System.out.println("Фрагмент: " + uri.getFragment());
+        System.out.println("------------------");
+        URL url = new uri.toURL();
+        URLConnection urlConnection = url.openConnection();
+        urlConnection.addRequestProperty();
+        urlConnection.connect();
+        Scanner scanner = new Scanner(urlConnection.getInputStream());
+//        Scanner scanner = new Scanner(url.openStream());
+        while (scanner.hasNextLine()) {
+            System.out.println(scanner.nextLine());
+        }
+        System.out.println("------------------");
+        URL resource =URIRunner.class.getResource("/data.csv");
+        System.out.println(resource.toURI().getScheme());
+        System.out.println(resource);
     }
+
 }
