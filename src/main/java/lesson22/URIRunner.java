85968f5f984782b599444b1810e9ed042cd01738
antonshishkin92 <antonshishkin92@gmail.com>
2022-01-26 17:41:24 +0400

    lesson 22: java.net

--- src/main/java/lesson22/URIRunner.java
+++ src/main/java/lesson22/URIRunner.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 
 public class URIRunner {
     public static void main(String[] args) throws URISyntaxException, IOException {
-        URI uri = new URI();
+        URI uri = new URI("https://www.rbc.ru/rbcfreenews/61f10e6d9a7947ec6efa2931?utm_source=yxnews&utm_medium=desktop");
         System.out.println("Протокол:" + uri.getScheme());
         System.out.println("Хост: " + uri.getHost());
         System.out.println("Порт: " + uri.getPort());
@@ -17,9 +17,9 @@ public class URIRunner {
         System.out.println("Параметры: " + uri.getRawQuery());
         System.out.println("Фрагмент: " + uri.getFragment());
         System.out.println("------------------");
-        URL url = new uri.toURL();
+        URL url = uri.toURL();
         URLConnection urlConnection = url.openConnection();
-        urlConnection.addRequestProperty();
+        urlConnection.addRequestProperty("User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 OPR/82.0.4227.58");
         urlConnection.connect();
         Scanner scanner = new Scanner(urlConnection.getInputStream());
 //        Scanner scanner = new Scanner(url.openStream());
@@ -31,5 +31,6 @@ public class URIRunner {
         System.out.println("Протокол: " + resource.toURI().getScheme());
         System.out.println("Адрес ресурса: " + resource);
     }
+
 
 }
