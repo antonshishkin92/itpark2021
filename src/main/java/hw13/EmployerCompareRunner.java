3e149e82f995c13968cd5769c7fddb35c76b09f9
antonshishkin92 <antonshishkin92@gmail.com>
2022-01-30 15:42:36 +0400

    hw13: lesson 13

--- src/main/java/hw13/EmployerCompareRunner.java
+++ src/main/java/hw13/EmployerCompareRunner.java
@@ -6,7 +6,7 @@ import java.util.*;
 public class EmployerCompareRunner {
 
     public static void main(String[] args) {
-        Set<Employer> employers = new LinkedHashSet<>();
+        List<Employer> employers = new LinkedList<>();
         employers.add(new Employer(1, "Васильев Анатолий Сергеевич", 15));
         employers.add(new Employer(119, "Петров Федор Андреевич", 7));
         employers.add(new Employer(98, "Боширов Валентин Антонович", 23));
@@ -15,7 +15,6 @@ public class EmployerCompareRunner {
         employers.add(new Employer(6, "Березина Мария Степановна", 5));
         employers.add(new Employer(237, "Мякишева Анна Сергеевна", 12));
         System.out.println(employers);
-
 
     }
 }
