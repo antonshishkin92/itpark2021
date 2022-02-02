8f3de997a801dbd8cfc82b87a376bea0bee0ddb0
antonshishkin92 <antonshishkin92@gmail.com>
2022-02-02 16:11:48 +0400

    ДЗ 22 к уроку №22

---
+++ src/main/java/hw22/dto/WeatherDto.java
@@ -0,0 +1,21 @@
+package hw22.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class WeatherDto {
+
+    @JsonProperty("main")
+    private MainDto main;
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public class MainDto {
+        private Double temp;
+    }
+
+}
