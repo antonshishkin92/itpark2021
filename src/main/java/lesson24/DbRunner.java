a3f8429ebc0753dc651f308cb1642219c1a228f7
antonshishkin92 <antonshishkin92@gmail.com>
2022-02-04 15:13:25 +0400

    lesson 24:JDBC

--- src/main/java/lesson24/DbRunner.java
+++ src/main/java/lesson24/DbRunner.java
@@ -3,7 +3,13 @@ package lesson24;
 import lesson24.dto.Employee;
 
 import java.io.IOException;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Savepoint;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -32,18 +38,22 @@ public class DbRunner {
              final Statement st = connection.createStatement();
              final PreparedStatement statement = connection.prepareStatement(
                      """
-                     select e.emp_id, e.emp_name, e.salary, d.name department_name
-                     from employee e
-                     left join department d
-                     on e.department_id = d.id
-                     where e.emp_id = ?
-                     """)// -1 or (d)
+                             select e.emp_id, e.emp_name, e.salary, d.name department_name 
+                             from employee e 
+                             left join department d 
+                             on e.department_id = d.id 
+                             where e.emp_id = ?
+                             """) // -1 or (d)
         ) {
             connection.setAutoCommit(false);
             Savepoint beforeCreate = connection.setSavepoint("beforeCreate");
+
             st.execute("insert into department (id, name) values (156, 'New')");
+
             Savepoint beforeUpdate = connection.setSavepoint("beforeUpdate");
+
             st.execute("update department set name = 'New2' where id = 156");
+
             statement.setInt(1, id);
             List<Employee> employees = new ArrayList<>();
             try (final ResultSet resultSet = statement.executeQuery()) {
@@ -61,4 +71,5 @@ public class DbRunner {
         }
     }
 }
+
 
