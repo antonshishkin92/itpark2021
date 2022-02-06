362936bd55fb6f92077a869a08a94b8af888a77b
antonshishkin92 <antonshishkin92@gmail.com>
2022-02-06 17:13:56 +0400

    lesson 25:Spring

--- src/main/java/lesson25/spring/service/impl/PersonServiceImpl.java
+++ src/main/java/lesson25/spring/service/impl/PersonServiceImpl.java
@@ -3,7 +3,9 @@ package lesson25.spring.service.impl;
 import lesson25.spring.dao.PersonDao;
 import lesson25.spring.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+//@Component
 public class PersonServiceImpl implements PersonService {
     private final PersonDao personDao;
 
