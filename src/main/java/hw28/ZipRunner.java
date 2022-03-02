e47d8908bbd14c20b81d9744bc65adb69d16e5f5
antonshishkin92 <antonshishkin92@gmail.com>
2022-03-02 17:12:41 +0400

    hw28 to Lesson 28

---
+++ src/main/java/hw28/ZipRunner.java
@@ -0,0 +1,28 @@
+package hw28;
+
+import hw28.service.ArchiveService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class ZipRunner {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Укажите параметры запуска программы и перезапустите ее");
+            return;
+        }
+        System.out.println("Запуск архиватора...");
+        final ConfigurableApplicationContext applicationContext = SpringApplication.run(ZipRunner.class, args);
+        final ArchiveService archiveService = applicationContext.getBean(ArchiveService.class);
+        Arrays.stream(args).findFirst().ifPresentOrElse(file -> {
+            final String zip = archiveService.zip(file).orElseThrow(() -> new IllegalStateException("Не удалось выполнить архивацию"));
+            System.out.printf("Архиватор успешно выполнил свою работу. Результат %s", zip);
+        }, () -> {
+            System.out.println("Укажите архивируемый файл в качестве параметра запуска программы");
+            System.exit(0);
+        });
+    }
+}
