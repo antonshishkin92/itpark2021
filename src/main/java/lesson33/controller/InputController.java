2bc0e8ab6928679f518ebcfddc04fff25cf08abb
antonshishkin92 <antonshishkin92@gmail.com>
2022-03-18 21:33:25 +0400

    lesson 33 feat: Продолжаем изучать spring web mvc

---
+++ src/main/java/lesson33/controller/InputController.java
@@ -0,0 +1,26 @@
+package lesson33.controller;
+
+import lesson33.service.CalculatorService;
+import lesson33.dto.ResultDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/calculator")
+public class InputController {
+    @Value("${spring.application.name}")
+    private String application;
+
+    private final CalculatorService calculatorService;
+
+    //    @RequestMapping()
+    @PostMapping("/summa")
+    public ResultDto summa(@RequestParam("arg1") Integer a, @RequestParam("arg2") Integer b) {
+        return new ResultDto(application, calculatorService.summa(a, b));
+    }
+}
