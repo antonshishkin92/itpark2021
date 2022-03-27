d81b83caac64c306f4701331b8a8b11b580799f9
antonshishkin92 <antonshishkin92@gmail.com>
2022-03-27 17:12:17 +0400

    lesson 25 feat: Join fetch + web

--- src/main/java/lesson25/entity/Employee.java
+++ src/main/java/lesson25/entity/Employee.java
@@ -2,6 +2,7 @@ package lesson25.entity;
 
 
 import lombok.Data;
+import lombok.ToString;
 
 
 import javax.persistence.*;
@@ -10,7 +11,12 @@ import java.math.BigDecimal;
 @Data
 @Entity
 @Table(name = "employee")
+@ToString(exclude = "department")
+@NamedEntityGraph(name = Employee.DEPARTMENT_GRAPH_NAME, attributeNodes = {@NamedAttributeNode("department")})
 public class Employee {
+
+    public static final String DEPARTMENT_GRAPH_NAME = "Employee.department";
+
     @Id
     @Column(name = "emp_id")
     @GeneratedValue//(strategy = GenerationType.AUTO)
@@ -21,7 +27,7 @@ public class Employee {
 
     private BigDecimal salary;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "department_id")
     private Department department;
 }
