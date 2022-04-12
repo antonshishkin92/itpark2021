4a2e80179bf0e77628de61e5cb0a1098a2a1e905
antonshishkin92 <antonshishkin92@gmail.com>
2022-04-12 21:11:28 +0400

    lesson 36: support multi-module project

---
+++ src/main/java/lesson36/Config/AppConfig.java
@@ -0,0 +1,20 @@
+package lesson36.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+    // First Method: default
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+
+//    // Second Method: Using RestTemplateBuilder
+//    @Bean
+//    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+//        return builder.build();
+//    }
+}
