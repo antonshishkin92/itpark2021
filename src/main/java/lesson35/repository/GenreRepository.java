c3f57c5134f0bb2e8e3ec6681de06125b433257d
antonshishkin92 <antonshishkin92@gmail.com>
2022-04-21 16:36:09 +0400

    lesson36: support multi-module project

--- src/main/java/lesson35/repository/GenreRepository.java
+++
@@ -1,5 +0,0 @@
-package lesson35.repository;
-import lesson35.model.Genre;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface GenreRepository extends JpaRepository<Genre,String>{
-}
