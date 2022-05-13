8ddc46c494ef0901f40d868e7dee75400de50f9e
antonshishkin92 <antonshishkin92@gmail.com>
2022-05-13 15:36:20 +0400

    my project

--- src/main/java/antonProject/repository/CarRepository.java
+++ src/main/java/antonProject/repository/CarRepository.java
@@ -1,27 +1,8 @@
 package antonProject.repository;
 
 import antonProject.model.Car;
-import antonProject.repository.dto.CarParamDto;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import javax.validation.constraints.NotNull;
-import java.util.List;
 
 public interface CarRepository extends JpaRepository<Car, Integer> {
-    @Query("select c from Car c" +
-            " join fetch c.category categ" +
-            " join fetch c.brand b" +
-            " join fetch c.numberState n" +
-            " join fetch c.nameOwner nam" +
-            " join fetch c.surnameOwner s" +
-            " join fetch c.region r" +
-            " where (:#{#paramDto.carId} is null or c.id = :#{#paramDto.carId}) and " +
-            "(:#{#paramDto.category} is null or c.category = :#{#paramDto.category}) and " +
-            "(:#{#paramDto.brand} is null or lower(c.brand) like concat('%', :#{#paramDto.brand} , '%')) and" +
-            "(:#{#paramDto.nameOwner} is null or c.nameOwner =:#{#paramDto.nameOwner}) and" +
-            "(:#{#paramDto.surnameOwner} is null or lower(c.surnameOwner) like concat('%', :#{#paramDto.surnameOwner} , '%')) and" +
-            "(:#{#paramDto.regionCode} is null or c.region =:#{#paramDto.regionCode}) and" +
-            "(:#{#paramDto.numberState} is null or lower(c.numberState) like concat('%', :#{#paramDto.numberState}, '%'))")
-    List<Car> getByParams(@NotNull CarParamDto paramDto);
+
 }
--- src/main/java/antonProject/repository/RegionRepository.java
+++ src/main/java/antonProject/repository/RegionRepository.java
@@ -1,18 +1,8 @@
 package antonProject.repository;
 
 import antonProject.model.Region;
-import antonProject.repository.dto.RegionParamDto;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import javax.validation.constraints.NotNull;
-import java.util.List;
 
 public interface RegionRepository extends JpaRepository<Region, Integer> {
-    @Query("select r from Region r" +
-            " join fetch r.code code" +
-            " join fetch r.nameRegion name" +
-            " where (:#{#paramDto.codeRegion} is null or r.code = :#{#paramDto.codeRegion}) and" +
-            "(:#{#paramDto.nameRegion} is null or r.nameRegion =:#{#paramDto.nameRegion})")
-    List<Region> getByParams(@NotNull RegionParamDto paramDto);
+
 }
