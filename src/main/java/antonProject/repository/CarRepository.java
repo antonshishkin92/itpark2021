6b1f4fd01ab76784b13b138c16bd1c1effec8368
antonshishkin92 <antonshishkin92@gmail.com>
2022-05-16 10:41:38 +0400

    delete project

--- src/main/java/antonProject/repository/CarRepository.java
+++
@@ -1,8 +0,0 @@
-package antonProject.repository;
-
-import antonProject.model.Car;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CarRepository extends JpaRepository<Car, Integer> {
-
-}
--- src/main/java/antonProject/repository/RegionRepository.java
+++
@@ -1,8 +0,0 @@
-package antonProject.repository;
-
-import antonProject.model.Region;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RegionRepository extends JpaRepository<Region, Integer> {
-
-}
